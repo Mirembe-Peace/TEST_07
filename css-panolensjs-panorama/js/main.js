fd1e21ec1db3be0f6345e77eed74f64055e6fd45
Peace Mercy <peace2de@gmail.com>
2025-03-03 11:32:14 +0300

    test_07 1: loading pano success         2: testing loading 3D-model

--- css-panolensjs-panorama/js/main.js
+++ css-panolensjs-panorama/js/main.js
@@ -1,3 +1,4 @@
+import { GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 const pan = document.querySelector('.pan'); //select panolens div
 const img = 'images/pan_1.jpg';
 
@@ -13,3 +14,11 @@ viewer.add(panorama);
 
 console.log(PANOLENS.VERSION);
 console.log(PANOLENS.THREE_VERSION);
+
+//loading the 3D model
+const loader = new GLTFLoader();    
+loader.load('3D_assets/ANKLE RATTLES.glb', function (gltf) {
+    scenes.add(gltf.scene);
+}, undefined, function (error){
+    console.error(error);   
+});
